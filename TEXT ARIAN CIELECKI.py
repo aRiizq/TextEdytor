e92089fe15012a4d3c07785360844f32739bbfff
aRiizq <mrpanda0071@gmail.com>
2022-11-15 19:16:07 +0100

    updated formating, finishing touches es

--- TEXT ARIAN CIELECKI.py
+++ TEXT ARIAN CIELECKI.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont
 from PyQt5 import uic
 
+
 class MyGUI(QMainWindow):
 
     def __init__(self):
@@ -19,8 +20,6 @@ class MyGUI(QMainWindow):
         self.actionClose.triggered.connect(self.close)
         self.actionChange_Font.triggered.connect(self.change_font)
 
-
-
     def change_size(self, size):
         self.plainTextEdit.setFont(QFont("Arial", size))
 
@@ -31,22 +30,26 @@ class MyGUI(QMainWindow):
 
     def open_file(self):
         options = QFileDialog.Options()
-        filename, _ = QFileDialog.getOpenFileName(self, "Open File","","Text Files (*.txt);;Python Files(*.py)",options=options)
+        filename, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Text Files (*.txt);;Python Files(*.py)",
+                                                  options=options)
         if filename != "":
             with open(filename, "r") as f:
-               self.plainTextEdit.setPlainText(f.read())
+                self.plainTextEdit.setPlainText(f.read())
+
     def save_file(self):
         options = QFileDialog.Options()
-        filename, _ = QFileDialog.getSaveFileName(self, "Save File","","Text Files (*.txt);;All Files(*)",options=options)
+        filename, _ = QFileDialog.getSaveFileName(self, "Save File", "", "Text Files (*.txt);;All Files(*)",
+                                                  options=options)
         if filename != "":
             with open(filename, "w") as f:
                 f.write(self.plainTextEdit.toPlainText())
+
     def closeEvent(self, event):
         dialog = QMessageBox()
         dialog.setText("Do you want to save your file?")
-        dialog.addButton(QPushButton("Yes"), QMessageBox.YesRole) #0
-        dialog.addButton(QPushButton("No"), QMessageBox.NoRole) #1
-        dialog.addButton(QPushButton("Cancel"), QMessageBox.RejectRole) #2
+        dialog.addButton(QPushButton("Yes"), QMessageBox.YesRole)  # 0
+        dialog.addButton(QPushButton("No"), QMessageBox.NoRole)  # 1
+        dialog.addButton(QPushButton("Cancel"), QMessageBox.RejectRole)  # 2
 
         answer = dialog.exec_()
         if answer == 0:
@@ -55,11 +58,12 @@ class MyGUI(QMainWindow):
         elif answer == 2:
             event.ignore()
 
-def main ():
+
+def main():
     app = QApplication([])
     window = MyGUI()
     app.exec_()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
